ccec39b35d819dcb804a2f15608b27eb58c0eb51
iron-orc <pal-ork@ukr.net>
2014-11-04 12:42:54 +0200

    delete some trash

--- main.cpp
+++ main.cpp
@@ -4,27 +4,17 @@
 #include <iomanip>
 
 void stack_search(int v,int newv){
-std::cout << *((int*)0x28ff18) << std::endl;
 	int ebp,esp;
 	 __asm__ volatile (
 		  "mov %%fs:0x8, %0 \n"
 		  "mov %%fs:0x4, %1 \n"
           : "=r" (esp), "=r" (ebp));
 
-	std::cout <<sizeof(int)<<std::endl;
-	std::cout << std::hex << esp << " " << ebp << std::endl;;
-//	for(int i = esp; i <=ebp; i+=1){
-	//	if(i % 8 == 0) std::cout << std::endl;
-	//	std::cout << std::hex << (short)*((unsigned char*)i) <<"\t";
-	//}	  
 	for(int i = esp; i <=ebp; i+=sizeof(int)){
-		//if(i % 8 == 0) std::cout << std::endl;
-		//std::cout << std::dec << *((int*)i) <<"\t";
-		//if( i == 0x28ff18) std::cout <<std::dec <<*((int*)i) <<" " <<v<< " LOL\n";
 		if(v == *((int*)i) && &v != ((int*)i)){
 			*((int*)i) = newv;
-			std::cout << "STACK FOUND!" << *((int*)i) << std::hex << ((int*)i)<< std::endl;
-		//	break;
+			//std::cout << "STACK FOUND!" << *((int*)i) << std::hex << ((int*)i)<< std::endl;
+			//make this part a more funny
 		}
 	}	  
 }
@@ -34,7 +24,6 @@ void heap_search(int v,int newv){
 	int NumberOfHeaps = GetProcessHeaps(0, NULL);
 	aHeaps = new HANDLE[NumberOfHeaps];
 	GetProcessHeaps(NumberOfHeaps, aHeaps);
-	std::cout <<"\n"<< NumberOfHeaps << std::endl; 	
 	for(int i = 1; i < NumberOfHeaps; ++i){
 		phe.lpData = NULL;
 		std::cout << "HEAP #" << i << std::endl;
@@ -47,7 +36,7 @@ void heap_search(int v,int newv){
 				//	std::cout << std::hex << phe.wFlags << std::endl;
 					if(v == *((int*)j)){
 						*((int*)j) = newv;
-						std::cout << "Heap FOUND!" << std::endl;
+	//					std::cout << "Heap FOUND!" << std::endl;
 						goto _exit;
 						break;
 					}
